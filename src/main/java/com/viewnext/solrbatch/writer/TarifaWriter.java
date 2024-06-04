6aee376d95f5d8c6a525af508baac3f4950b8d8c
aojalvog <alvaro.ojalvo@gmail.com>
2024-06-04 10:15:31 +0200

    feat(model y writer): :sparkles:

---
+++ src/main/java/com/viewnext/solrbatch/writer/TarifaWriter.java
@@ -0,0 +1,13 @@
+package com.viewnext.solrbatch.writer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Component
+@Slf4j
+public class TarifaWriter {
+
+}
