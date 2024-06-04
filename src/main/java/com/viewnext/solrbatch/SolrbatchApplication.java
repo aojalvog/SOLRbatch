7cc1a4daf7607f312dbc6bf40b72887c27244cff
aojalvog <alvaro.ojalvo@gmail.com>
2024-06-04 09:46:45 +0200

    inicio

---
+++ src/main/java/com/viewnext/solrbatch/SolrbatchApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.solrbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewnext/solrbatch/SolrbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewnext.solrbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
